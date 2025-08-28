b4e2bc3093b6edc3283502b0571a14ed9d28a510
Victor <lvictor255@protonmail.com>
2025-08-27 22:00:35 -0300

    Created a function to load routes; Created a factory function that returns http handlers that serve pages

--- src/main.go
+++ src/main.go
@@ -3,30 +3,22 @@ package main
 import (
 	"context"
 	"log"
-	"net/http"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httprate"
-	"github.com/vl0000/gomessenger/gen/messaging/v1/messagingv1connect"
 	"github.com/vl0000/gomessenger/server"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
 	s := server.New()
 
-	path, handler := messagingv1connect.NewMessagingServiceHandler(s)
-
 	s.Router.Use(middleware.Logger)
 	s.Router.Use(middleware.Timeout(5 * time.Second))
 	s.Router.Use(httprate.LimitByIP(100, 1*time.Minute))
-
-	s.Router.Handle(path+"*", h2c.NewHandler(handler, &http2.Server{}))
-	s.Router.Handle("/*", http.FileServer(http.Dir("./public/_app/")))
+	s.LoadRoutes()
 
 	go func() {
 		err := s.Run()
---
+++ src/server/routes.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/vl0000/gomessenger/gen/messaging/v1/messagingv1connect"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+func ServeHTML(path string) http.HandlerFunc {
+	if page, err := os.ReadFile(path); err != nil {
+		log.Printf("%s Not found\n", path)
+	} else {
+
+		return func(w http.ResponseWriter, r *http.Request) {
+			_, err = w.Write(page)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		http.Error(w, "Not Found", 404)
+	}
+}
+
+func (s *MessagingServer) LoadRoutes() {
+
+	// Loads static files for svelte apps
+	s.Router.Handle("/*", http.FileServer(http.Dir("./public/")))
+
+	// Loads the paths for the messaging service
+	path, handler := messagingv1connect.NewMessagingServiceHandler(s)
+	s.Router.Handle(path+"*", h2c.NewHandler(handler, &http2.Server{}))
+
+	s.Router.Get("/", ServeHTML("./public/login.html"))
+	s.Router.Get("/signup", ServeHTML("./public/signup.html"))
+	s.Router.Get("/chat", ServeHTML("./public/chat.html"))
+}
